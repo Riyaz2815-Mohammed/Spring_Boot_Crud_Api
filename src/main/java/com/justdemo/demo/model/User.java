3af0b21d2e62f726701f20664f22a2743caa0d5c
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2026-04-23 16:54:03 +0530

    Created the model User that is The base model for the upcoming user object and given set and get functionality for future purpose

--- src/main/java/com/justdemo/demo/model/User.java
+++ src/main/java/com/justdemo/demo/model/User.java
@@ -3,18 +3,40 @@ package com.justdemo.demo.model;
 public class User {
     String name;
     Integer age;
-    String Dept;
+    String dept;
 
-    User(String n,Integer a,String d){
-        this.name=n;
-        this.age=a;
-        this.Dept=d;
+//    User(){}
+public User() {
+}
 
-        public getUser(User u){
-            return u.name + " - " + u.age + " - " + u.dept;
-        }
+    // Parameterized constructor (optional but useful)
+    public User(String name, Integer age, String dept) {
+        this.name = name;
+        this.age = age;
+        this.dept = dept;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
+    public void setDept(String dept) {
+        this.dept=dept;
 
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public String getName() {
+        return name;
     }
 }
